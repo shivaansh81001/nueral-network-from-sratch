a2cd8c29994535f99d4018ef3606b7fb81b7d510
shivaansh81001 <shivaans@ualberta.ca>
2025-02-15 22:48:32 -0700

    Activation class

--- Activation.py
+++ Activation.py
@@ -15,3 +15,27 @@ class Activation:
     def tanh(self):
         '''Returns tanh actiavted results'''
         return np.tanh(self.Z)
+
+    def softmax(self):
+        '''Returns the softmax probabilty distribution results'''
+        if self.Z.ndim==1:
+            num = np.exp(self.Z - np.max(self.Z))
+            return num/np.sum(num)
+        else:
+            num = np.exp(self.Z - np.max(self.Z,axis=1, keepdims=True))
+            return num/np.sum(num, axis=1, keepdims=True)
+    
+
+def test():
+    inputs = np.array([-2.1,3.1,1.1])
+    act = Activation(inputs)
+    print("softmax = ",act.softmax())
+
+    print("ReLU = ",act.ReLU())
+
+    print("sigmoid = ",act.sigmoid())
+
+    print("Tanh = ",act.tanh())
+
+if __name__== '__main__':
+    test()
